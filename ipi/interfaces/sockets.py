c0b0ae0a7633a45657959c57960e4690ee1e46a2
Johan McQuillan <johan.mcquillan.13@ucl.ac.uk>
2018-03-21 21:15:13 +0000

    Set step timer to only print for high verbosity

--- ipi/interfaces/sockets.py
+++ ipi/interfaces/sockets.py
@@ -661,7 +661,7 @@ class InterfaceSocket(object):
                 r["status"] = "Done"
                 r["t_finished"] = time.time()
                 dt = r["t_finished"] - r["t_dispatched"]
-                info(" @CUSTOM:    Time of current step = {} s".format(dt))
+                info(" @CUSTOM:    Time of current step = {} s".format(dt), verbosity.high)
                 c.lastreq = r["id"]  # saves the ID of the request that the client has just processed
                 self.jobs = [w for w in self.jobs if not (w[0] is r and w[1] is c)]  # removes pair in a robust way
 
